b1da6e2e7a9061434ab6cd1d766f304b44b2f860
cdedessu <clement.dedessuslesmous@student.vinci.be>
2025-05-01 09:08:40 +0000

    norminettage main.c

--- srcs/main/main.c
+++ srcs/main/main.c
@@ -6,25 +6,31 @@
 /*   By: cdedessu <cdedessu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 14:37:18 by jmaizel           #+#    #+#             */
-/*   Updated: 2025/04/30 18:03:36 by cdedessu         ###   ########.fr       */
+/*   Updated: 2025/05/01 09:06:23 by cdedessu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
 /**
- * Initialise les hooks pour la gestion des événements
+ * Initialise les paramètres de l'arme et du jeu
  * 
  * @param game Structure principale du jeu
  */
-static void	setup_hooks(t_game *game)
+static void	init_game_part_two(t_game *game)
 {
-	mlx_hook(game->win, 2, 1L << 0, key_press, game);
-	mlx_hook(game->win, 3, 1L << 1, key_release, game);
-	mlx_hook(game->win, 17, 0, close_window, game);
-	mlx_hook(game->win, 6, 1L << 6, mouse_move, game);
-	mlx_mouse_hook(game->win, mouse_click, game);
-	mlx_loop_hook(game->mlx, game_loop, game);
+	game->weapon_damage = 25;
+	game->weapon_range = 3.0;
+	game->weapon_frame_count = 0;
+	game->current_weapon_frame = 0;
+	game->weapon_animating = 0;
+	game->weapon_anim_time = 0;
+	game->weapon_anim_speed = 0.1;
+	game->door_opened = 0;
+	game->all_monsters_killed = 0;
+	game->victory_displayed = 0;
+	game->victory_timer = 0.0;
+	game->victory_final = 0;
 }
 
 /**
@@ -49,18 +55,22 @@ static void	init_game_structure(t_game *game)
 	game->firing = 0;
 	game->weapon_cooldown = 0.5;
 	game->weapon_timer = 0.0;
-	game->weapon_damage = 25;
-	game->weapon_range = 3.0;
-	game->weapon_frame_count = 0;
-	game->current_weapon_frame = 0;
-	game->weapon_animating = 0;
-	game->weapon_anim_time = 0;
-	game->weapon_anim_speed = 0.1;
-	game->door_opened = 0;
-	game->all_monsters_killed = 0;
-	game->victory_displayed = 0;
-	game->victory_timer = 0.0;
-	game->victory_final = 0;
+	init_game_part_two(game);
+}
+
+/**
+ * Initialise les hooks pour la gestion des événements
+ * 
+ * @param game Structure principale du jeu
+ */
+static void	setup_hooks(t_game *game)
+{
+	mlx_hook(game->win, 2, 1L << 0, key_press, game);
+	mlx_hook(game->win, 3, 1L << 1, key_release, game);
+	mlx_hook(game->win, 17, 0, close_window, game);
+	mlx_hook(game->win, 6, 1L << 6, mouse_move, game);
+	mlx_mouse_hook(game->win, mouse_click, game);
+	mlx_loop_hook(game->mlx, game_loop, game);
 }
 
 /**
